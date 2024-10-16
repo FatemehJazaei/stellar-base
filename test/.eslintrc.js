d6513f286a4f950be233b8884c76fadfd7052a78
Fatima <jazaeifateme@gmail.com>
2024-10-16 14:37:14 +0330

    stellar-base-master clone

---
+++ test/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    mocha: true
+  },
+  parser: '@babel/eslint-parser',
+  plugins: ['@babel', 'prettier', 'prefer-import'],
+  globals: {
+    StellarBase: true,
+    chai: true,
+    sinon: true,
+    expect: true
+  },
+  rules: {
+    'no-unused-vars': 0
+  }
+};
