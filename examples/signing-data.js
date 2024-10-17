364b21599134799eaa35fc79192cc583ec19fa9d
Fatima <jazaeifateme@gmail.com>
2024-10-17 12:48:29 +0330

    make file
    again

--- examples/signing-data.js
+++ examples/signing-data.js
@@ -3,29 +3,30 @@
 const StellarBase = require('../lib/index.js'); 
 
 var key = StellarBase.Keypair.random();
+console.log(key.xdrPublicKey());
 
-console.log('Keypair: ' );
-console.log(key);
+// console.log('Keypair: ' );
+// console.log(key);
 
-console.log('rawPublicKey: ' );
-console.log(key.rawPublicKey());
-console.log('publicKey: ' );
-console.log(key.publicKey());
+// console.log('rawPublicKey: ' );
+// console.log(key.rawPublicKey());
+// console.log('publicKey: ' );
+// console.log(key.publicKey());
 
 
-console.log('rawSecretKey: ' );
-console.log(key.rawSecretKey());
-console.log('secret: ' );
-console.log(key.secret());
+// console.log('rawSecretKey: ' );
+// console.log(key.rawSecretKey());
+// console.log('secret: ' );
+// console.log(key.secret());
 
-var data = 'data to sign';
-var signature = key.sign(data);
+// var data = 'data to sign';
+// var signature = key.sign(data);
 
-console.log('Signature: ' );
-console.log(signature);
+// console.log('Signature: ' );
+// console.log(signature);
 
-if (StellarBase.verify(data, signature, key.rawPublicKey())) {
-  console.log('OK!');
-} else {
-  console.log('Bad signature!');
-}
+// if (StellarBase.verify(data, signature, key.rawPublicKey())) {
+//   console.log('OK!');
+// } else {
+//   console.log('Bad signature!');
+// }
